995ea2252eb283c5da84dc3352a1ee5a3cac98b1
genesisbanzon <mysteriousrazor330@gmail.com>
2022-08-29 15:08:51 +0000

    dundundundududn wewwewewewwewe wewwwwwwwww
    DAADADADAwewewewewewdunununuun,dundundundundundundundundundn
    dundudnundundundWEWwewewewewewewewwwwwwwwwwwwwwwwwwwwwwwwww
    dudadaddadadadaaaaaaaaaaaaaaaaaaaaaaa
    dadadadadadadadaaaaaaaaaaaa
    dadaadadadadaaaaaaaaaaaaaaaaa
    dadadadadadadadaaaaaaaaaaaaa
    bmhchchcbmttttcmttttbmttttbmttttbmttttbmtttBAM
    ttttttgngngngngngngngngngngngngngngngngnggngngng
    gngngngngngngnngngngngnn
    (picture noises)
    dundundundun,dudnudundundunundundund,dundudnundundundWEWwewewewewewewewwwwwwwwwwwwwwwwwwwwwwwwwwwewwwwww
    DADADADA
    
    Signed-off-by: genesisbanzon <mysteriousrazor330@gmail.com>

--- brodom.cpp
+++ brodom.cpp
@@ -2,4 +2,14 @@
 
 int main()
 
+bool isDed = true
+
 std::cout << never gonna give you up, never gonna let you down, << std::endl;
+std::cout << idk how the std works << std::endl;
+
+while (isDed = true) {
+
+std::cout << never gonna run around, and desert you << std::endl;
+
+}
+
